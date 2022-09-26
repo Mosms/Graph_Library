0528bdd10c3ec0d7677c6cbba59c939d0f5442e5
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-26 19:29:01 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -18,7 +18,7 @@ int main()
     assert(g.AddEdge(1,1)==true);
     assert(g.GetDegree(1)==4);
     assert(g.RemoveEdge(3,1)==true);
-    for(auto i : g.GetNeighbors().size())
+    for(auto i : g.GetNeighbors())
         std::cout<<i<<std::endl;
     return 0;
 }
