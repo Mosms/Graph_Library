7f09c9c92206a73b47cb34da99ad66378636dd44
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-26 17:59:12 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -10,6 +10,9 @@ int main()
     assert(g.AddEdge(2,1)==true);
     assert(g.ContainsEdge(1,2)==true);
     assert(g.AddEdge(1,2)== false);
+    assert(g.AddEdge(1,3)== false);
+    assert(g.AddVertex(3)==true);
+    assert(g.AddEdge(1,3)== true);
 
     return 0;
 }
