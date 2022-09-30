c0925640dbeed99df0bb4c008f7be924e018e8a3
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-09-30 11:48:00 +0800

    compile

--- source/DataStructures/UndirectedWeightedGraph.cpp
+++ source/DataStructures/UndirectedWeightedGraph.cpp
@@ -1,16 +1,3 @@
 #include <DataStructures/UndirectedWeightedGraph.h>
 
 
-bool UndirectedWeightedGraph::RemoveEdge(int vertex1, int vertex2)
-
-bool UndirectedWeightedGraph::ContainsEdge(int vertex1, int vertex2) const
-
-int UndirectedWeightedGraph::GetWeight(int vertex1, int vertex2) const
-
-std::vector<WeightedEdge> UndirectedWeightedGraph::GetIncomingEdges(int vertex) const
-
-std::vector<WeightedEdge> UndirectedWeightedGraph::GetOutgoingEdges(int vertex) const
-
-int UndirectedWeightedGraph::GetDegree(int vertex) const
-
-std::vector<int> UndirectedWeightedGraph::GetNeighbors(int vertex) const
