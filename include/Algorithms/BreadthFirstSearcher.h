1ab4aeed661a0b25ed975a8f8940f3849371db13
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-10-30 21:31:00 +0800

    compile

--- include/Algorithms/BreadthFirstSearcher.h
+++ include/Algorithms/BreadthFirstSearcher.h
@@ -3,6 +3,9 @@
 
 #include <functional>
 #include <optional>
+#include <vector>
+#include <set>
+#include <queue>
 
 template <typename TGraph>
 class BreadthFirstSearcher {
@@ -10,5 +13,41 @@ public:
     static void VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action);
     static std::optional<int> FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate);
 };
+template <typename TGraph>
+void BreadthFirstSearcher<TGraph>::VisitAllVertices(const TGraph *graph, int start, std::function<void(int)> action) {
+    std::set<int> visited;
+    std::queue<int> ToVisit({start});
+    while(!ToVisit.empty()){
+        int nowVisit = ToVisit.front();
+        ToVisit.pop();
+        visited.emplace(nowVisit);
+
+        if(graph->ContainsVertex(nowVisit))
+            action(nowVisit);
+        for(auto cor : graph->GetNeighbors(nowVisit))
+            if(!visited.count(cor))
+                ToVisit.push(cor);
+    }
+    return;
+}
+
+template <typename TGraph>
+std::optional<int>
+BreadthFirstSearcher<TGraph>::FindFirstVertex(const TGraph *graph, int start, std::function<bool(int)> predicate) {
+    std::set<int> visited;
+    std::queue<int> ToVisit({start});
+    while(!ToVisit.empty()){
+        int nowVisit = ToVisit.front();
+        ToVisit.pop();
+        visited.emplace(nowVisit);
+
+        if(graph->ContainsVertex(nowVisit) && predicate(nowVisit))
+            return nowVisit;
+        for(auto cor : graph->GetNeighbors(nowVisit))
+            if(!visited.count(cor))
+                ToVisit.push(cor);
+    }
+    return std::nullopt;
+}
 
 #endif
