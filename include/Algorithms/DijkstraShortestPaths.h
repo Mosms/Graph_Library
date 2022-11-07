a7a17ecc93b863d54f5f4eb542b1c1ec59bc7560
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-07 19:47:47 +0800

    compile

--- include/Algorithms/DijkstraShortestPaths.h
+++ include/Algorithms/DijkstraShortestPaths.h
@@ -17,6 +17,7 @@ DijkstraShortestPaths<TGraph, TValue>::DijkstraShortestPaths(const TGraph<TValue
     auto CompareDValue = [](std::pair<int, TValue> &a, std::pair<int, TValue> &b)->bool{
         return a.second < b.second;
     };
+    std::set<int> Visited;
     std::priority_queue<std::pair<int, TValue>, std::vector<std::pair<int, TValue>>, decltype(CompareDValue)> ExtractMin(CompareDValue);
     DijkstraShortestPaths<TGraph, TValue>::VertexDValue.insert(std::make_pair(source, TValue()));
     ExtractMin.push(std::make_pair(source, TValue()));
@@ -24,6 +25,9 @@ DijkstraShortestPaths<TGraph, TValue>::DijkstraShortestPaths(const TGraph<TValue
         auto nowVisit = ExtractMin.top();
         ExtractMin.pop();
 
+        if(Visited.count(nowVisit.first))
+            continue;
+        else Visited.insert(nowVisit.first);
 
         for(auto cor : graph->GetOutgoingEdges(nowVisit.first)){
             auto ValueDNew = DijkstraShortestPaths<TGraph, TValue>::VertexDValue.find(cor.GetSource())->second + cor.GetWeight();
