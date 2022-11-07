588679863ba692b0d39c415b2ddd4bdabca88bd6
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-07 18:08:28 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -23,7 +23,7 @@ static void test1() {
     for (int i = 1; i <= 6; ++i) {
         p = new DijkstraShortestPaths<WeightedGraph, int>(g, i);
         for (int j = 1; j <= 6; ++j) {
-            printf("%d", p->HasPathTo(j));
+            printf("%d\n", p->HasPathTo(j));
             if(p->TryGetShortestPathTo(j) != std::nullopt)
                 for(auto m : *p->TryGetShortestPathTo(j))
                     printf("%d\n", m);
