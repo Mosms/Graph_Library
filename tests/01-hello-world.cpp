42c066480280372e1812057e5065d1613091b1e3
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-23 23:28:41 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -5,9 +5,10 @@
 #include <Algorithms/ShortestPaths.h>
 #include <Algorithms/DijkstraShortestPaths.h>
 #include <Algorithms/BellmanFordShortestPaths.h>
-#include <cassert>
-#include <iostream>
-#include <cstring>
+#include <cstdio>
+//#include <cassert>
+//#include <iostream>
+//#include <cstring>
 static void test1() {
     auto *g = new WeightedGraph<int>();
     for (int i = 1; i <= 6; ++i) {
@@ -63,68 +64,6 @@ class bb{
 };
 int main()
 {
-    UndirectedGraph g; // 创建一张新的图
-    assert(g.AddVertex(1)==true);
-    assert(g.AddVertex(2)==true);
-    assert(g.AddEdge(2,1)==true);
-    assert(g.ContainsEdge(1,2)==true);
-    assert(g.AddEdge(1,2)== false);
-    assert(g.AddEdge(1,3)== false);
-    assert(g.AddVertex(3)==true);
-    assert(g.AddEdge(1,3)== true);
-    assert(g.ContainsEdge(3,1)==true);
-    assert(g.GetDegree(1)==2);
-    assert(g.GetDegree(2)==1);
-    assert(g.GetDegree(3)==1);
-    assert(g.AddEdge(1,1)==true);
-    assert(g.GetDegree(1)==4);
-    assert(g.RemoveEdge(3,1)==true);
-    for(auto i : g.GetNeighbors(1))
-        std::cout<<i<<std::endl;
-    std::cout<<g.GetDegree(1)<<std::endl;
-
-    UndirectedWeightedGraph<std::string> w;
-    assert(w.AddVertex(1)==true);
-    assert(w.AddVertex(2)==true);
-    assert(w.AddVertex(3)==true);
-    assert(w.RemoveEdge(1,2)==false);
-    assert(w.ContainsEdge(1,2)==false);
-    assert(w.AddEdge(1,2,"yes")==true);
-    assert(w.AddEdge(1,2,"no")==false);
-    assert(w.AddEdge(1,3,"no")==true);
-    assert(w.GetWeight(2,1)=="yes");
-    for(auto i : w.GetNeighbors(1))
-        std::cout<<i<<std::endl;
-    for(auto i : w.GetIncomingEdges(1))
-        std::cout<<i.GetDestination()<<" "<<i.GetSource()<<" "<<i.GetWeight()<<std::endl;
-    for(auto i : w.GetOutgoingEdges(1))
-        std::cout<<i.GetDestination()<<" "<<i.GetSource()<<" "<<i.GetWeight()<<std::endl;
-    assert(w.ContainsEdge(1,2)==true);
-    assert(w.CountEdges()==2);
-    assert(w.RemoveEdge(1,2)==true);
-    for(auto i : w.GetNeighbors(1))
-        std::cout<<i<<std::endl;
-    for(auto i : w.GetIncomingEdges(1))
-        std::cout<<i.GetDestination()<<" "<<i.GetSource()<<" "<<i.GetWeight()<<std::endl;
-    for(auto i : w.GetOutgoingEdges(1))
-        std::cout<<i.GetDestination()<<" "<<i.GetSource()<<" "<<i.GetWeight()<<std::endl;
-
-    Graph r;
-    assert(r.AddVertex(1)==true);
-    assert(r.AddVertex(2)==true);
-    assert(r.AddVertex(3)==true);
-    assert(r.AddEdge(1,2)==true);
-    assert(r.AddEdge(1,3)==true);
-    for(auto cor : r.GetNeighbors(1))
-        std::cout << cor << std::endl;
-
-    std::function<void(int)> prin = [](int a){ printf("%d", a); };
-
-    BreadthFirstSearcher<Graph> check1;
-    check1.VisitAllVertices(&r, 1, prin);
-    DepthFirstSearcher<Graph> check2;
-    check2.VisitAllVertices(&r, 1, prin);
-    printf("\n");
 
     //WeightedGraph<bb> sw;
     //ShortestPaths<WeightedGraph<bb>> jskajak(&sw, 1);
