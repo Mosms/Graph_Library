848fc48ade0cb8c49510f81bc408ceebde505daa
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-23 20:16:23 +0800

    compile

--- include/Algorithms/ShortestPaths.h
+++ include/Algorithms/ShortestPaths.h
@@ -43,7 +43,7 @@ void ShortestPaths<TGraph>::ShortestPathAddVertex(int vertex, std::vector<int> *
 }
 template <class TGraph>
         ShortestPaths<TGraph>::ShortestPaths(const TGraph *graph, int source): Source(source) {
-    static_assert(std::is_same<decltype(typename TGraph::EdgeValueType()), std::true_type>::value,
+    static_assert(std::is_default_constructible(typename TGraph::EdgeValueType),
                   "TValue requires default constructor");
 }
 template <class TGraph>
