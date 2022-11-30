3a683e209cd660596d94de21316cb7258c035a21
tracer-ps3-2021 <tracer-ps3-2021@nju.edu.cn>
2022-11-30 18:25:35 +0800

    compile

--- tests/01-hello-world.cpp
+++ tests/01-hello-world.cpp
@@ -60,6 +60,31 @@ static void test2() {
 
     delete g;
 }
+static void test3() {
+    auto *g = new WeightedGraph<int>();
+    for (int i = 1; i <= 6; ++i) {
+        g->AddVertex(i);
+    }
+    g->AddEdge(1, 2, 1);
+    g->AddEdge(2, 3, 2);
+    g->AddEdge(3, 4, 3);
+    g->AddEdge(4, 1, 4);
+    g->AddEdge(5, 6, 5);
+    g->AddEdge(6, 5, 6);
+
+    MultiSourceShortestPaths<WeightedGraph<int>> *p = nullptr;
+    p = new FloydShortestPaths<WeightedGraph<int>>(g);
+    for (int i = 1; i <= 6; ++i) {
+        for (int j = 1; j <= 6; ++j){
+            printf("%d", p->HasPathOf(i, j));
+        }
+
+        printf("\n");
+        delete p;
+    }
+
+    delete g;
+}
 class bb{
     bb()=delete;
 };
@@ -72,5 +97,6 @@ int main()
 
     test1();
     test2();
+    printf("\n");
     return 0;
 }
